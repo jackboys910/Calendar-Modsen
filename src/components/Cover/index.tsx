87fa4bcf6a999879c66cee02358efcc2f2f29e8c
jackboys910 <george.jackboys@gmail.com>
2025-03-08 19:11:48 +0300

    feat: switched from use to the older and more stable import on SCSS, moved constants from components to the constants folder, added the AuthLayout component

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -1,11 +1,13 @@
+import { BRAND_NAME, COVER_TEXT } from '@constants/authPageText';
+
 import styles from './index.module.scss';
 
 const Cover: React.FC = () => {
   return (
     <div className={styles.container}>
       <header className={styles['greeting-container']}>
-        <h2 className={styles['greeting-text']}>Welcome</h2>
-        <p className={styles['greeting-description']}>your day is in your hands</p>
+        <h2 className={styles['greeting-text']}>{COVER_TEXT.GREETING_TEXT}</h2>
+        <p className={styles['greeting-description']}>{COVER_TEXT.GREETING_DESCRIPTION}</p>
       </header>
       <section className={styles.content}>
         <nav className={styles.pagination}>
@@ -16,8 +18,8 @@ const Cover: React.FC = () => {
           </ul>
         </nav>
         <article className={styles.article}>
-          <h1 className={styles['article-title']}>Modsen Calendar</h1>
-          <p className={styles['article-text']}>Use a calendar to plan your work day and week.</p>
+          <h1 className={styles['article-title']}>{BRAND_NAME}</h1>
+          <p className={styles['article-text']}>{COVER_TEXT.ARTICLE_TEXT}</p>
         </article>
       </section>
     </div>
---
+++ src/constants/authPageText.ts
@@ -0,0 +1,15 @@
+export const COVER_TEXT = {
+  GREETING_TEXT: 'Welcome',
+  GREETING_DESCRIPTION: 'your day is in your hands',
+  ARTICLE_TEXT: 'Use a calendar to plan your work day and week.',
+};
+
+export const BRAND_NAME = 'Modsen Calendar';
+
+export const AUTH_LAYOUT_TEXT = {
+  LAYOUT_REGISTATION_TITLE: 'Create Your Account',
+  LAYOUT_REGISTATION_TEXT: 'Already have an account? ',
+  LAYOUT_LOGIN_TITLE: 'Login',
+  LAYOUT_LOGIN_TEXT: 'Don`t have an account? ',
+  LAYOUT_LOGIN_TOGGLE: 'Create Now',
+};
